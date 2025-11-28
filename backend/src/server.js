853067019ea7d461d65215c118d7d1d0bdb3a8a7
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2025-11-28 18:42:54 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 
 const port = process.env.PORT;
@@ -9,6 +12,20 @@ app.get("/" , (req,res) => {
         message : "hello world"
     })
 })
+
+app.get("/books" , (req,res) => {
+    res.status(201).json({
+        message : "this is books endpoint"
+    })
+})
+
+if(ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    })
+}
+
 app.listen(ENV.PORT, () => {
     console.log("listening on port 3000")
 })
